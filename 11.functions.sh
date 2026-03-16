c96f76f182a2e5abb176dc733211d24c6eee6651
Rani Akshaya <vasaraniakshaya@gmail.com>
2026-03-16 15:38:53 +0530

    bash

---
+++ 11.functions.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+userid=$(id -u)
+
+if [ $userid -ne 0 ]; then
+  echo "Please run this script with the root user access"
+  exit 1
+fi
+
+VALIDATE(){
+  if [ $1 -ne 0 ]; then
+     echo "$2... FAILURE"
+     exit 1
+  else
+     echo "$2.. SUCCESS"
+ fi
+}
+
+echo "Installing Nginx"
+dnf install nginx -y
+VALIDATE $? "Installing Nginx"
+
+echo "Installing MYSQL"
+dnf install mysql -y
+VALIDATE $? "Installing MYSQL"
+
+echo "Installing NodqJs"
+dnf install nodejs -y
+VALIDATE $? "Installing NodeJs"
