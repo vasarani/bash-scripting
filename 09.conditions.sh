8a8eeb5236e738dd72fcce7d00e6ad8f228dde1b
Rani Akshaya <vasaraniakshaya@gmail.com>
2026-03-16 13:08:11 +0530

    bash

--- 09.conditions.sh
+++ 09.conditions.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
-number=25
+number=$1
 
 # -gt -> greater than
 # -lt -> less than
 # -eq -> equal 
 # -ne -> not equal
 
-if [ $number -gt 40 ]; then
+if [ $number -gt 20 ]; then
   echo "Given number: $number is greater than 20"
+else [ $number -eq 20 ]; then
+  echo "Given number: $number is equal to 20"
 else
   echo "Given number: $number is less than 20"
 fi
