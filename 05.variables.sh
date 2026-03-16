1f5c0e83b312a0537246714194077e58cf119bed
Rani Akshaya <vasaraniakshaya@gmail.com>
2026-03-16 11:15:23 +0530

    bash

---
+++ 05.variables.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo " PLease enter your name here: "
+read "USER_NAME"
+
+echo "your name is $User_NAME"
+
+echo " Enter your Password: "
+read -s "PASSWORD"
+
+echo "password is $PASSWORD"
