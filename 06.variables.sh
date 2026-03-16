0ef88c2c0cdf0645b3d2a5cd0e885aa312bddf28
Rani Akshaya <vasaraniakshaya@gmail.com>
2026-03-16 11:40:37 +0530

    bash

--- 06.variables.sh
+++ 06.variables.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-START_TIME=$(date)
+START_TIME=$(date +%s)
 echo "Script excuted: $START_TIME"
 
 sleep 10
 
-END_TIME=$(date)
+END_TIME=$(date +%s)
 TOTAL_TIME=$(($END_TIME-$START_TIME))
 
 echo "Script excuted: $TOTAL_TIME"
