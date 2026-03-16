43c4914e1dd9aa58399facbf87d1f3654f41da16
Rani Akshaya <vasaraniakshaya@gmail.com>
2026-03-16 16:51:40 +0530

    bash

---
+++ 15-loops.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+userid=$(id -u)
+logs_folder="/var/log/bash-scripting"
+logs_file="/var/log/bash-scripting/$0.logs"
+
+if [ $userid -ne 0 ]; then
+  echo "Please run this script with the root user access"
+  exit 1
+fi 
+mkdir -p $logs_folder
+VALIDATE(){
+    if [ $1 -ne 0 ]; then
+      echo "$2.... FAILURE" |tee -a $logs_file
+    else
+      echo "$2.... SUCCESS"  |tee -a $logs_file
+    fi 
+}
+
+
+for package in $@ # sudo sh file_name nginx nodejs mysql
+do 
+  dnf list installed $package &>> $logs_file
+  if [ $? -ne 0 ]; then
+    echo "$package not installed, installing now"
+    dnf install $package -y &>> $logs_file
+    VALIDATE $? "$package  Installed"
+  else
+    echo "$package already installed, Skipping"
+  fi     
+done  
