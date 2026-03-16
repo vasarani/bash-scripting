7de53d1b1692b307d9b8d30c8758ab143c0e1c74
Rani Akshaya <vasaraniakshaya@gmail.com>
2026-03-16 11:08:03 +0530

    bash

---
+++ 04.variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+person1=$1
+person2=$2
+
+echo "$person1:: Hi $person2, how are you ?"
+echo "$person2:: Hey hi $person1, I'm good. Long time no see. what's going?"
+echo "$person1:: Yeah All good $person2"
+echo "$person2:: Soo Nice"
