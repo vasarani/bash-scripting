da37ed094a16a9345b9a0912b1ae4a1b32f21200
Rani Akshaya <vasaraniakshaya@gmail.com>
2026-03-16 15:21:51 +0530

    bash

--- 10.conditions.sh
+++ 10.conditions.sh
@@ -14,3 +14,20 @@ if [ $? -ne 0 ]; then
 else
    echo "Installing Nginx... SUCCESS" 
 fi   
+
+echo "Installing MYSQL"
+dnf install mysql -y
+if [ $? -ne 0 ]; then 
+  echo "Installing mysql... FAILURE"
+else
+  echo "Installing mysql... SUCCESS"
+fi
+
+echo "Installing Nodq.Js"
+dnf install node.js -y
+if [ $? -ne 0 ]; then
+  echo "Installing Node.js... FAILURE"
+else
+  echo "Installing Node.js... SUCCESS"
+fi
+
